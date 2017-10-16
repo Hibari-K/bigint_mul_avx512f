467afe4c074383d4d208dc1c5657c308186c764a
Hibari-K <smiya_revaken@yahoo.co.jp>
2017-10-16 10:12:26 +0900

    add var interval

--- time.sh
+++ time.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-i=15
-N=55
+init=4
+
+interval=4
+i=$init
+N=56
 
 loop=$1
-st=`expr \( $N - $i \) \/ 5 + 1 + 2`	#115
+st=`expr \( $N - $i \) \/ $interval + 1 + 2`	#115
 en=`expr $loop + $st - 1`	#214
 
 ### initialize ###
@@ -18,14 +21,14 @@ while [ $i -le $N ]
 do
 	bit=`expr $i \* 128`
 	echo "$bit,=average(A$st:A$en),=average(B$st:B$en),=average(C$st:C$en)">>measure.csv
-	i=`expr $i + 5`
+	i=`expr $i + $interval`
 	st=`expr $st + $loop`
 	en=`expr $en + $loop`
 done
 
 ### measure ###
 
-outer=15
+outer=$init
 
 cat hoge | sed "s/128 \* 1/128 \* $outer/" > zmm_mul.h
 make clean all
@@ -36,11 +39,11 @@ do
 	while [ $i -lt $loop ]
 	do
 		echo "$outer : $i"
-		./mul `perl -e "print '11111111'x(4*$outer-1)"` >> measure.csv
+		./mul `perl -e "print 'ffffffff'x(4*$outer)"` >> measure.csv
 		sleep 0.1s
 		i=`expr $i + 1`
 	done
-	outer=`expr $outer + 5`
+	outer=`expr $outer + $interval`
 	cat hoge | sed "s/128 \* 1/128 \* $outer/" > zmm_mul.h
 	make clean all
 done
